67f15ec153ff3fc1e10e3f8cf166f5b2395ecc07
Ondrej Hulík <hulik.ondrej@gmail.com>
2024-12-21 18:59:43 +0100

    Add more tests

--- main.rb
+++ main.rb
@@ -4,48 +4,48 @@ require_relative 'lib/hashmap'
 
 map = HashMap.new
 
-# Set some key-value pairs
 map.print_entries
 map.set('name', 'Carlos')
 map.print_entries
 map.set('age', 22)
 map.print_entries
 map.set('city', 'Madrid')
+map.set('building', 'house')
+map.set('kitchen', 'modern')
+map.set('bathroom', 'need of repairs')
 map.print_entries
 
-# Get values for the keys
 map.get('name')
 map.get('age')
 map.get('city')
 
-# Try to get a non-existent key (should return nil)
 map.get('country')
 
-# Set more keys to test collision handling
 map.set('new_name', 'Carlos Jr.')
 map.set('new_age', 23)
 
-# Test getting values for newly added keys
 map.get('new_name')
 map.get('new_age')
 
-# Test collision: Both 'name' and 'new_name' might be in the same bucket
 map.get('name')
 map.get('new_name')
 
-# Test a non-existent key after several operations
 map.get('random_key')
 
-# Checking if the map contains certain keys (example placeholder)
 p map.has?('name')
 p map.has?('random_key')
 
-# Check the current state of the map (printing the HashMap)
-# map.print_entries
+p map.remove('name')
 
-# Add more values to test size and operations
+map.print_entries
+p map.length
+
 map.set('country', 'Spain')
 map.set('language', 'Spanish')
 
-# Retrieve the updated map as a string
-# map.print_entries
+p map.keys
+p map.values
+
+map.print_entries
+map.clear
+map.print_entries
